d8179f0d11f656e9a38026b6148c4689888af718
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-10-28 14:40:53 +0300

    Feat: Describe the structure of the ModelingPipeline by defining methods and parameters

--- scripts/modeling.py
+++ scripts/modeling.py
@@ -21,7 +21,10 @@ class ModelingPipeline:
         self.x_test = x_test
         self.y_train = y_train
         self.y_test = y_test
-    
+
+        self.experiment_name = None
+        self.experiment_id = None
+
     def initialize_mlflow(self, tracking_uri:str) -> None:
         """
         A method for defining the mlflow tracking uri
@@ -33,7 +36,7 @@ class ModelingPipeline:
         # Set the tracking URI
         mlflow.set_tracking_uri(uri=tracking_uri)
     
-    def create_experiment(self, experiment_name:str) -> str:
+    def create_experiment(self, experiment_name:str, artifact_path:str) -> str:
         """
         A method that will create an experiment in mlflow and return its id.
         If an experiment with the name already exists it will return the id of it.
@@ -52,5 +55,60 @@ class ModelingPipeline:
             search_result = mlflow.search_experiments(filter_string=f"name = '{experiment_name}'")
             experiment_id = search_result[0].experiment_id
         
+        self.experiment_id = experiment_id
         return experiment_id
 
+    def train_logistic_regressor(self) -> None:
+        """
+        A method that trains a logistic regressor for the dataset stored in the instance.
+        """
+
+    def train_decision_tree(self) -> None:
+        """
+        A method that trains a decision tree for the dataset stored in the instance.
+        """
+
+    def train_random_forset(self) -> None:
+        """
+        A method that trains a random forst for the dataset stored in the instance.
+        """
+
+    def train_gradient_booster(self) -> None:
+        """
+        A method that trains a gradient booster for the dataset stored in the instance.
+        """
+
+    def train_mlp(self) -> None:
+        """
+        A method that trains a multi level perceptron for the dataset stored in the instance.
+        """
+
+    def train_cnn(self) -> None:
+        """
+        A method that trains a cnn for the dataset stored in the instance.
+        """
+
+    def train_rnn(self) -> None:
+        """
+        A method that trains a rnn for the dataset stored in the instance.
+        """
+    
+    def train_lstm(self) -> None:
+        """
+        A method that trains a lstm for the dataset stored in the instance.
+        """
+
+    def log_best_model(self, export_path:str) -> None:
+        """
+        A method that will search for the best performing model from the experiment and then save it to a specified path.
+
+        Args:
+            export_path(str): the path where you want the best model to be moved/copied to
+        """
+
+    def train_models(self):
+        """
+        A method to trigger training for all of the models present.
+        **Note:** you can add more methods to train specific models then add the method inside to be included with all of the other models.
+        """
+    
