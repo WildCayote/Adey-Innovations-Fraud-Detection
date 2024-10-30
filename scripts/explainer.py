f22892df6d114584c12337dd3e9aec587eaffbdf
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-10-30 12:06:47 +0300

    Feat: Added a class for modularizing model explanation tasks

---
+++ scripts/explainer.py
@@ -0,0 +1,11 @@
+
+
+class ModelExplainer:
+    """
+    A class which uses SHAP and LIME to explain the decision of models.
+    """
+
+    def __init__(self):
+        """
+        Creates an instance of the class
+        """
